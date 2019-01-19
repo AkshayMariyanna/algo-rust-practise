671be40ab2f1a43567cf8f25594e5fe9f2ecf15c
Akshay Mariyanna <akshay.mariyanna@gmail.com>
2019-01-19 22:49:58 +0530

    GCD LCM

--- algebra/src/gcd.rs
+++ algebra/src/gcd.rs
@@ -22,3 +22,33 @@ pub fn gcd(a: u64, b: u64) -> u64 {
         _ => gcd(b, a % b),
     }
 }
+
+/// GCD(a, b)
+///
+/// [CP - Algos](https://cp-algorithms.com/algebra/euclid-algorithm.html)
+/// gcd(a, b) = {
+///   if b = 0 => a
+///   else     => gcd(b, a mod b)
+/// }
+///
+/// Euclidean algorithm
+/// ```
+/// let a = 25;
+/// let b = 10;
+/// assert_eq!(algebra::gcd_non_recursive(a, b), 5);
+///
+/// let a = 15;
+/// let b = 16;
+/// assert_eq!(algebra::gcd_non_recursive(a, b), 1);
+/// ```
+pub fn gcd_non_recursive(mut a: u64, mut b: u64) -> u64 {
+    while b != 0 {
+        a = a % b;
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+
+    a
+}
+
--- algebra/src/lib.rs
+++ algebra/src/lib.rs
@@ -9,3 +9,15 @@ mod gcd;
 pub use binary_exponentiation::*;
 pub use geometric_progression::*;
 pub use gcd::*;
+
+/// LCM(a, b)
+///
+/// ```
+/// let a = 10;
+/// let b = 15;
+///
+/// assert_eq!(algebra::lcm(a, b), 30);
+/// ```
+pub fn lcm(a: u64, b: u64) -> u64 {
+    (a / gcd(a, b)) * b
+}
